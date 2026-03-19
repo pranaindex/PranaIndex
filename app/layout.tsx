608dd7aa8512504d66758e1f3b3e0f5061ca881f
pranaindex <founder@pranaindex.com>
2026-03-19 20:22:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import './globals.css';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Prana Index',
-  description: 'Find your Prana Index',
-}
+  description: 'Master Your Focus.',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">{children}</body>
+      {/* This ensures the dark background applies everywhere */}
+      <body className="bg-slate-900 text-white antialiased">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
