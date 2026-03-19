cb1d5b485604bbbcb71bb98c60a95b87bab38be7
pranaindex <founder@pranaindex.com>
2026-03-19 14:42:37 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,1 @@
-import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Prana Index",
-  description: "Find your Pi stress score",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className="antialiased">{children}</body>
-    </html>
-  );
-}
