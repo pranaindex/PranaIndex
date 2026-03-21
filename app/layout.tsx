cd67cdedb4c699133c258acd194fa35031a5f1f5
pranaindex <founder@pranaindex.com>
2026-03-21 10:02:03 +0530

    updated package.json and layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,21 @@
-import './globals.css'
+import React from 'react';
+import './globals.css';
+
+export const metadata = {
+  title: 'Prana Index',
+  description: 'Breathing and Calibration',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, backgroundColor: 'black', color: 'white' }}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
